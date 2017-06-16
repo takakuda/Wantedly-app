7a58f8a6068fe744cd4e5031379e9a4ad9f50e6c
kei <kei.takakuda.0415@icloud.com>
2017-06-16 16:56:50 +0900

    Add js

--- app/assets/javascripts/calender.js
+++ app/assets/javascripts/calender.js
@@ -2,3 +2,14 @@ $(document).ready(function() {
     $('#calendar').fullCalendar({
     })
 });
+
+$(function() {
+  $(".calendar-btn").on("click", function() {
+    $("#modal-content").show();
+  });
+});
+$(function() {
+  $(".menu-btn").on("click", function() {
+    $("#modal-content").hide();
+  });
+});
