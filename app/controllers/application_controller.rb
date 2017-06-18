1073bfe79582509a92aae3b53561da431ac3387f
kei <kei.takakuda.0415@icloud.com>
2017-06-19 04:58:06 +0900

    change sanitizer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :campany_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :campany_name, :user_name])
   end
 end
