c5dc5b2a5a200eff9cc47852753e5344b749aedd
kei <kei.takakuda.0415@icloud.com>
2017-06-19 04:57:41 +0900

    Add campany_id to applies controller

--- app/controllers/applies_controller.rb
+++ app/controllers/applies_controller.rb
@@ -1,12 +1,12 @@
 class AppliesController < ApplicationController
   def create
-    @apply = Apply.create(recruitment_id: params[:recruitment_id], user_id: current_user.id)
+    @apply = Apply.create(recruitment_id: params[:recruitment_id], user_id: current_user.id, campany_id: params[:campany_id], user_name: current_user.user_name )
     redirect_to root_path
   end
 
   private
   def apply_params
-    params.permit(:recruitment_id)
+    params.permit(:recruitment_id, :campany_id )
   end
 
 end
