1ad19cb313f33a995a1fea020cb88889ac90e2f5
kei <kei.takakuda.0415@icloud.com>
2017-06-20 17:50:08 +0900

    Change carrier wave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,8 +6,8 @@ CarrierWave.configure do |config|
   config.storage = :fog
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV['ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
+    aws_access_key_id:  Rails.application.secrets.aws_access_key_id,
+    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
     region: 'ap-northeast-1'
   }
 
--- vendor/bundle/ruby/2.3.0/gems/capistrano-3.8.1/lib/capistrano.rb
+++ vendor/bundle/ruby/2.3.0/gems/capistrano-3.8.1/lib/capistrano.rb
@@ -1,6 +1,1 @@
-set :default_env, {
-  rbenv_root: "/usr/local/rbenv",
-  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
-  aws_access_key_id: ENV['ACCESS_KEY_ID'],
-  aws_secret_access_key: ENV['SECRET_ACCESS_KEY']
-}
+
