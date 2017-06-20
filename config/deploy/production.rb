eeab943c56b6d7623a4eaf874fa16f50954e7616
kei <kei.takakuda.0415@icloud.com>
2017-06-20 13:00:33 +0900

    Deploy production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,5 @@
-server '54.65.90.246', user: 'ec2-user', roles: %w{app db web}
+server '54.65.90.246', user: 'takakudakei', roles: %w{app db web}
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
