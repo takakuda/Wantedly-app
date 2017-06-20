1cd41b7e44f0f7d6a7c47022bca98f23a0c3c558
kei <kei.takakuda.0415@icloud.com>
2017-06-20 12:44:02 +0900

    Deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '<Elastic IP>', user: 'ec2-user', roles: %w{app db web}
+server '54.65.90.246', user: 'ec2-user', roles: %w{app db web}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
